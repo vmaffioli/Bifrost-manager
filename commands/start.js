0479d1939432fed651e13fe1e2b6bb39b87c20ce
vmaffioli <vinimaffioli@gmail.com>
2021-01-07 19:26:54 +0000

    add start cmd

--- commands/start.js
+++ commands/start.js
@@ -1,3 +1,32 @@
-const database = require("../database")
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
 
-database.createProfile();
+const adapter = new FileSync('profile_data.json')
+const db = low(adapter)
+
+const guildName = 'Nome da guilda'
+const playerName = 'Nome do Jogador'
+const characterInfos = {
+  name: "Nome do Personagem",
+  job: "Profissão do Personagem",
+  description: "Descrição do Personagem",
+  lore: "História do Personagem",
+  photo: "Imagem do Personagem"
+
+}
+
+
+  //create
+db.set(guildName, []).write()
+
+//post
+db.get(guildName).push({
+    player_name: playerName,
+    character_name: characterInfos["name"],
+    character_job: characterInfos["job"],
+    character_description: characterInfos["description"],
+    character_lore: characterInfos["lore"],
+    character_photo: characterInfos["photo"]
+  }).write()
+
+
--- database.js
+++ database.js
@@ -5,28 +5,9 @@ const adapter = new FileSync('profile_data.json')
 const db = low(adapter)
 
 const guildName = 'Nome da guilda'
-const playerName = 'Nome do Jogador'
-const characterInfos = {
-  name: "Nome do Personagem",
-  job: "Profissão do Personagem",
-  description: "Descrição do Personagem",
-  lore: "História do Personagem",
-  photo: "Imagem do Personagem"
 
-}
 
 module.exports.func = function createProfile(){
   //create
 db.set(guildName, []).write()
 
-//post
-db.get(guildName).push({
-    player_name: playerName,
-    character_name: characterInfos["name"],
-    character_job: characterInfos["job"],
-    character_description: characterInfos["description"],
-    character_lore: characterInfos["lore"],
-    character_photo: characterInfos["photo"]
-  }).write()
-
-}
