61748de61dd60872b8ca115e1e35a22f156fb79b
vmaffioli <vinimaffioli@gmail.com>
2021-01-07 19:51:07 +0000

    fixing

--- commands/start.js
+++ commands/start.js
@@ -1,30 +1,30 @@
+const Discord = require("discord.js");
 const low = require('lowdb')
 const FileSync = require('lowdb/adapters/FileSync')
-
 const adapter = new FileSync('profile_data.json')
 const db = low(adapter)
 
-const guildName = 'Nome da guilda'
-const playerName = 'Nome do Jogador'
-const characterInfos = {
-  name: "Nome do Personagem",
-  job: "Profissão do Personagem",
-  description: "Descrição do Personagem",
-  lore: "História do Personagem",
-  photo: "Imagem do Personagem"
-
-}
-const Discord = require("discord.js"); 
 
 exports.run = async (client, message, args) => {
+
+  const guildName = 'Nome da guilda'
+  const playerName = 'Nome do Jogador'
+  const characterInfos = {
+    name: "Nome do Personagem",
+    job: "Profissão do Personagem",
+    description: "Descrição do Personagem",
+    lore: "História do Personagem",
+    photo: "Imagem do Personagem"
+
+  }
 
 
 
   //create
-db.set(guildName, []).write()
+  db.set(guildName, []).write()
 
-//post
-db.get(guildName).push({
+  //post
+  db.get(guildName).push({
     player_name: playerName,
     character_name: characterInfos["name"],
     character_job: characterInfos["job"],
